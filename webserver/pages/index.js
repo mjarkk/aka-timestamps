7963ff2e9f62c7d359d97f14af7ec219730c5fe3
mjarkk <mkopenga@gmail.com>
2020-10-28 20:49:56 +0100

    Basic nextjs site

---
+++ webserver/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AKA timestamps</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>AKA timestamps</h1>
+      </main>
+    </>
+  )
+}
